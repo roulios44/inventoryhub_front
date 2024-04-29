e04f5d7c9b2b2b742e669c54b77bec7f5b34a0df
roulios44 <jules.croizier@gmail.com>
2024-04-29 11:10:33 +0200

    create header tempalte

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,134 +1,42 @@
 <template>
-    <div class="header">
-      <button @click="toggleMenu" :class="['burger-menu', { 'active': showMenu }]">
-        <span></span>
-        <span></span>
-        <span></span>
-      </button>
-  
-      <transition name="slide">
-        <ul v-if="showMenu" class="menu">
-          <li style="cursor:pointer" v-on:click="disconnect()">Se déconnecter</li>
+    <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <a class='navbar-brand' href='/'>LOGO</a>
+      <button aria-controls='navbarSupportedContent' aria-expanded='true' aria-label='Toggle navigation' class='navbar-toggler' data-bs-target='#navbarSupportedContent' data-bs-toggle='collapse' type='button'>
+          <span class='navbar-toggler-icon'></span>
+        </button>
+      <div class='collapse navbar-collapse' id='navbarSupportedContent'>
+        <ul class='navbar-nav me-auto mb-2 mb-lg-0'>
+          <li class='nav-item'><a aria-current='page' class='nav-link active' href='/'>Home</a></li>
+          <li class='nav-item'><a class='nav-link' href='#'>Link</a></li>
+          <li class='nav-item dropdown'><a aria-expanded='true' class='nav-link dropdown-toggle' data-bs-toggle='dropdown' href='#' role='button'>Dropdown</a>
+            <ul class='dropdown-menu'>
+              <li><a class='dropdown-item' href='#'>Action</a></li>
+              <li><a class='dropdown-item' href='#'>Another action</a></li>
+              <li>
+                <hr class='dropdown-divider' />
+              </li>
+              <li><a class='dropdown-item' href='#'>Something else here</a>
+              </li>
+            </ul>
+          </li>
         </ul>
-      </transition>
-      <div class="logo">
-        <img src="@/assets/logo.png">
-      </div>
-      <div class="sectionTitle">
-        <h1>Suivi Hebdo</h1>
+
       </div>
     </div>
-  </template>
+  </nav>
+</template>
   
   <script>
   export default {
     data() {
       return {
-        showMenu: false
       };
     },
     methods: {
-      toggleMenu() {
-        this.showMenu = !this.showMenu;
-      },
-      disconnect(){
-        localStorage.removeItem("userID")
-        this.$router.push("login")
-      }
     }
   };
   </script>
   
   <style>
-  .burger-menu {
-    margin: 1%;
-    box-shadow: #333;
-    position: relative;
-    z-index: 500;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    width: 75px;
-    border: none;
-    background-color: #fff;
-    height: 50px;
-    cursor: pointer;
-  }
-  
-  .burger-menu span {
-    background-color: #333;
-    width: 100%;
-    height: 10%;
-  }
-  
-  .menu {
-    position: fixed;
-    margin-top: 5%;
-    top: 0;
-    left: 0;
-    width: 250px;
-    height: 100%;
-    background-color: #fff;
-    box-shadow: 0px 5px 0px rgb(207, 189, 189);
-  
-    z-index: 998;
-    padding: 20px;
-  }
-  
-  .slide-enter-active,
-  .slide-leave-active {
-    transition: transform 0.3s ease-in-out;
-  }
-  
-  .slide-enter,
-  .slide-leave-to {
-    transform: translateX(-100%);
-  }
-  ul{
-    list-style-type: none;
-  }
-  
-  .header{
-    display: flex;
-      flex-direction: row;
-      flex-wrap: nowrap;
-      justify-content: flex-start;
-      align-items: stretch;
-      align-content: stretch;
-    margin: 0;
-    padding: 0;
-    width: 100%;
-    height: 35%;
-    min-height: 35px;
-    box-shadow: 0px 5px 0px rgb(207, 189, 189);
-  }
-  
-  .logo img{
-    width: 100%;
-    max-width: 200px;
-  }
-  .logo{
-    display: flex;
-      flex-direction: row;
-      flex-wrap: wrap;
-      justify-content: center;
-      align-items: center;
-      align-content: center;
-    margin: 0;
-    padding: 0;
-    width: 25%;
-    height: 50%;
-    max-width: fit-content;
-  }
-  
-  .sectionTitle{
-    margin-left: 5%;
-    display: flex;
-      flex-direction: row;
-      flex-wrap: wrap;
-      justify-content: center;
-      align-items: center;
-      align-content: center;
-  }
-  
   </style>
