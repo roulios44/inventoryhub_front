2dd559dbea45593ecba6a8c5bb81b5ad71b1ad34
roulios44 <jules.croizier@gmail.com>
2024-05-26 23:50:16 +0200

    push to work from home

---
+++ src/components/FournisseursComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    hello
+    <p v-for="fournisseur in fournisseurs">{{ fournisseur }}</p>
+</template>
+  
+  <script>
+  import axios from 'axios';
+  export default {
+    data() {
+      return {
+        fournisseurs : []
+      };
+    },
+    methods: {
+        async getAllFournisseurs(){
+          console.log("hello")
+            // const req = await axios.get("http://localhost:8080/fournisseurs",{
+            //   headers : {
+            //     'Access-Control-Allow-Origin': '*'
+            //   }
+            // })
+            // const res = await req.data
+            // this.fournisseurs = res
+            const req = await fetch("http://localhost:8080/fournisseurs")
+            const res = req.body
+            console.log(res)
+
+        },
+        async registerNewFournisseur(){
+
+        },
+    },
+    async mounted(){
+        this.getAllFournisseurs()
+    }
+  };
+  </script>
+  
+  <style>
+  </style>
---
+++ src/views/FournisseursView.vue
@@ -0,0 +1,7 @@
+<script setup>
+import FournisseursComponent from '../components/FournisseursComponent.vue';
+</script>
+
+<template>
+    <FournisseursComponent />
+</template>
