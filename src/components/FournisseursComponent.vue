d84b0b191737b5d10e716a92345d255861908336
roulios44 <jules.croizier@gmail.com>
2024-06-16 21:14:57 +0200

    adding some front object, TODO -> general page for consult and craetion of entity using update in backend

--- src/components/FournisseursComponent.vue
+++ src/components/FournisseursComponent.vue
@@ -1,40 +1,96 @@
 <template>
-    hello
-    <p v-for="fournisseur in fournisseurs">{{ fournisseur }}</p>
+  <div class="fournisseursHeader">
+    <div class="">
+      <h2>Liste des fournisseurs</h2>
+      <button v-on:click="redirectCreateFournisseur()">Créer un fournisseur</button>
+    </div>
+    <input type="text" placeholder="Search">
+  </div>
+  <ul class="list-group">
+    <li class="list-group-item active">Fournisseurs</li>
+    <li v-for="fournisseur in fournisseurs" class="list-group-item">
+      <div class="fournisseur-list">
+        {{ fournisseur.titre }}
+        {{ fournisseur.description }}
+        <button class="btn btn-danger" v-on:click="deleteFourniseur(fournisseur.id)">X</button>
+      </div>
+    
+    </li>
+  </ul>
+    <div class="createFournisseur">
+      <form @submit.prevent="registerNewFournisseur">
+        <input type="text" placeholder="titre" v-model="titre" required>
+        <input type="text" placeholder="adresse" v-model="adresse" required>
+        <input type="text" placeholder="code postal" v-model="postCode" required>
+        <input type="text" placeholder="description" v-model="description" required>
+        <input type="text" placeholder="mail" v-model="mail" required>
+        <input type="text" placeholder="country" v-model="country" required>
+        <input type="text" placeholder="tel" v-model="tel" required>
+        <button type="submit" v-on:click="registerNewFournisseur()">Create</button>
+      </form>
+    </div>
 </template>
   
-  <script>
-  import axios from 'axios';
-  export default {
-    data() {
-      return {
-        fournisseurs : []
-      };
-    },
-    methods: {
-        async getAllFournisseurs(){
-          console.log("hello")
-            // const req = await axios.get("http://localhost:8080/fournisseurs",{
-            //   headers : {
-            //     'Access-Control-Allow-Origin': '*'
-            //   }
-            // })
-            // const res = await req.data
-            // this.fournisseurs = res
-            const req = await fetch("http://localhost:8080/fournisseurs")
-            const res = req.body
-            console.log(res)
-
-        },
-        async registerNewFournisseur(){
-
-        },
-    },
-    async mounted(){
+<script>
+import axios from 'axios';
+const apiUrl = import.meta.env.VITE_API_URL
+const contextApiUrl = apiUrl + "/fournisseurs"
+export default {
+  data() {
+    return {
+      fournisseurs : [],
+      titre : null,
+      adresse : null,
+      postCode : null,
+      description : null,
+      mail : null,
+      country : null,
+      tel : null,
+    };
+  },
+  methods: {
+      async getAllFournisseurs(){
+          const req = await axios.get(contextApiUrl)
+          const res = await req.data
+          this.fournisseurs = res._embedded.fournisseurs
+      },
+      async registerNewFournisseur(){
+        const data = {
+          "titre" : this.titre,
+          "adresse" : this.adresse,
+          "code_postal	" : this.postCode,
+          "description" : this.description,
+          "mail" : this.mail,
+          "pays" : this.country,
+          "tel" : this.tel,
+        }
+        const req = await axios.post(contextApiUrl,data)
+        const res = await req.data
+        console.log(res)
         this.getAllFournisseurs()
-    }
-  };
-  </script>
-  
-  <style>
-  </style>
+      },
+      async deleteFourniseur(id){
+        const req = await axios.delete(contextApiUrl + "/" + id)
+        const res = await req.data
+        this.getAllFournisseurs()
+      },
+      async redirectCreateFournisseur(){
+        this.$router.push('/createFournisseur')
+      }
+  },
+  async mounted(){
+      this.getAllFournisseurs()
+  }
+};
+</script>
+<style>
+.fournisseursHeader{
+  background-color: grey;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  align-items: center;
+  flex-direction: row;
+}
+</style>
